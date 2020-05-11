12ed12112f1858a05b80880e56768f3385ff746c
DeokJo Jang <jdj9354@gmail.com>
2020-05-12 08:33:01 +0900

    Rest template for buy/accept

--- rest/v1/request.js
+++ rest/v1/request.js
@@ -1,13 +1,29 @@
-var express = require("express")
+let express = require('express')
+let bodyParser = require('body-parser')
+let logger = require('./../../logger')
 
-var router = express.Router()
+let router = express.Router()
+router.use(bodyParser.json())
 
-router.post("/buy", function (req, res) {
+router.post("/buy", (req, res) => {
+        let body = req.body
+        let tokenId = body.tokenId
+        let requester = body.requester
+        let signedTransaction = body.signedTransaction
+
+        logger.info(`tokenId : ${tokenId} requester : ${requester} signedTransaction : ${signedTransaction}`)
+        res.send()
     }
 )
 
-router.post("/accept", function (req, res) {
+router.post("/accept", (req, res) => {
+        let body = req.body
+        let tokenId = body.tokenId
+        let requester = body.requester
+
+        logger.info(`tokenId : ${tokenId} requester : ${requester}`)
+        res.send()
     }
-);
+)
 
 module.exports = router
